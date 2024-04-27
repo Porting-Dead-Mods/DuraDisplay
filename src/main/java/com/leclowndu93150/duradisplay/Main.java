0605ff1bb722b726f994a55c2176eb27991219fd
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-04-27 15:54:04 +0200

    Add Energy Compat

--- src/main/java/com/leclowndu93150/duradisplay/Main.java
+++ src/main/java/com/leclowndu93150/duradisplay/Main.java
@@ -11,8 +11,10 @@ import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.client.IItemDecorator;
 import net.neoforged.neoforge.client.event.RegisterItemDecorationsEvent;
+import net.neoforged.neoforge.energy.IEnergyStorage;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -35,7 +37,33 @@ public class Main
     private static class DuraDisplay implements IItemDecorator {
 
         public boolean render(GuiGraphics guiGraphics, Font font, ItemStack stack, int xPosition, int yPosition) {
-            if (!stack.isEmpty() && stack.isDamaged()) {
+
+            IEnergyStorage energyStorage = stack.getCapability(Capabilities.EnergyStorage.ITEM);
+            if (!stack.isEmpty() && energyStorage != null){
+                PoseStack poseStack = guiGraphics.pose();
+
+                int energyStored = energyStorage.getEnergyStored();
+                int maxEnergyStorage = energyStorage.getMaxEnergyStored();
+
+                double durabilityPercentage = ((double) (maxEnergyStorage - energyStored) / (double) maxEnergyStorage) * 100.0;
+
+                String formattedPercentage = String.format("%.0f%%", durabilityPercentage);
+
+                int stringWidth = font.width(formattedPercentage);
+                int x = ((xPosition + 8) * 2 + 1 + stringWidth / 2 - stringWidth);
+                int y = (yPosition * 2) + 22;
+
+                int color = 0x34D8EB;
+                poseStack.pushPose();
+                poseStack.scale(0.5F, 0.5F, 0.5F);
+                poseStack.translate(0.0D, 0.0D, 750.0F);
+                MultiBufferSource.BufferSource multibuffersource$buffersource = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
+                font.drawInBatch(formattedPercentage, x, y, color, true, poseStack.last().pose(), multibuffersource$buffersource, Font.DisplayMode.NORMAL, 0, 15728880, false);
+                multibuffersource$buffersource.endBatch();
+                poseStack.popPose();
+            }
+
+            if (!stack.isEmpty() && stack.isDamaged() && energyStorage == null) {
                 PoseStack poseStack = guiGraphics.pose();
 
                 int damage = stack.getDamageValue();
