e6be7788f72d6551d3c317e5ecddbd4d7789965b
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-04-30 18:48:12 +0200

    Revert "Partial GT Compat"
    This reverts commit 0b88f6e5

--- src/main/java/com/leclowndu93150/duradisplay/Main.java
+++ src/main/java/com/leclowndu93150/duradisplay/Main.java
@@ -87,8 +87,7 @@ public class Main
                 if (energyStorage.isPresent()) {
                     type = DisplayType.ENERGY;
                 }
-
-                if(euStorage != null){
+                if(euStorage!= null){
                     type = DisplayType.GREGTECH;
                 }
 
@@ -119,12 +118,14 @@ public class Main
                             guiGraphics.fill(RenderType.guiOverlay(), j, k, j + l, k + 1, i | 0xFF000000);
                         }
                         break;
+
                     case GREGTECH:
                                 long energyStored = euStorage.getAmount();
                                 long maxEnergyStorage = euStorage.getCapacity();
                                 double energyPercentage = ((double) energyStored / (double) maxEnergyStorage) * 100D;
                                 renderText(guiGraphics, font, String.format("%.0f%%", energyPercentage), xPosition, yPosition, 0x34D8EB); // Custom color for energy display
                         break;
+
                     case CUSTOM:
                         if (customDisplayItem != null && customDisplayItem.shouldDisplay(stack)) {
                             double energyPercentage = customDisplayItem.getPercentage(stack);
